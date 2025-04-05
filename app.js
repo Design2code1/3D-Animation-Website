3f199cb8720de17c87162b9254013bae2e1dfa9f
Design2Code <design2code1@gmail.com>
2025-04-05 11:31:07 +0530

    commit

--- app.js
+++ app.js
@@ -1,32 +1,110 @@
-let listBg = document.querySelectorAll('.bg');
-let listTab = document.querySelectorAll('.tab');
-let titleBanner = document.querySelector('.banner h1');
-window.addEventListener("scroll", (event) => {
-    /*scrollY is the web scrollbar position (pixel)*/
-    let top = this.scrollY;
-    /*index is the order of classes bg (0,1,2,3,4,5,6,7,8)
-    When scrolling the web, the classes bg scroll down,
-    the bigger the index, the faster the movement
-    */
-    listBg.forEach((bg, index) => {
-        if(index != 0 && index != 8){
-            bg.style.transform = `translateY(${(top*index/2)}px)`;
-        }else if(index == 0){
-            bg.style.transform = `translateY(${(top/3)}px)`;
-        }
-    })
-    titleBanner.style.transform = `translateY(${(top*4/2)}px)`;
+import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
+import { gsap } from 'https://cdn.skypack.dev/gsap';
 
-    /* parallax scroll in tab
-    when tab's position is less than 550 px
-    from scrollbar position add active class to animate 
-    and vice versa
-    */
-    listTab.forEach(tab =>{
-        if(tab.offsetTop - top < 550){
-            tab.classList.add('active');
-        }else{
-            tab.classList.remove('active');
+const camera = new THREE.PerspectiveCamera(
+    10,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+camera.position.z = 13;
+
+const scene = new THREE.Scene();
+let bee;
+let mixer;
+const loader = new GLTFLoader();
+loader.load('/demon_bee_full_texture.glb',
+    function (gltf) {
+        bee = gltf.scene;
+        scene.add(bee);
+
+        mixer = new THREE.AnimationMixer(bee);
+        mixer.clipAction(gltf.animations[0]).play();
+        modelMove();
+    },
+    function (xhr) {},
+    function (error) {}
+);
+const renderer = new THREE.WebGLRenderer({alpha: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById('container3D').appendChild(renderer.domElement);
+
+// light
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.3);
+scene.add(ambientLight);
+
+const topLight = new THREE.DirectionalLight(0xffffff, 1);
+topLight.position.set(500, 500, 500);
+scene.add(topLight);
+
+
+const reRender3D = () => {
+    requestAnimationFrame(reRender3D);
+    renderer.render(scene, camera);
+    if(mixer) mixer.update(0.02);
+};
+reRender3D();
+
+let arrPositionModel = [
+    {
+        id: 'banner',
+        position: {x: 0, y: -1, z: 0},
+        rotation: {x: 0, y: 1.5, z: 0}
+    },
+    {
+        id: "intro",
+        position: { x: 1, y: -1, z: -5 },
+        rotation: { x: 0.5, y: -0.5, z: 0 },
+    },
+    {
+        id: "description",
+        position: { x: -1, y: -1, z: -5 },
+        rotation: { x: 0, y: 0.5, z: 0 },
+    },
+    {
+        id: "contact",
+        position: { x: 0.8, y: -1, z: 0 },
+        rotation: { x: 0.3, y: -0.5, z: 0 },
+    },
+];
+const modelMove = () => {
+    const sections = document.querySelectorAll('.section');
+    let currentSection;
+    sections.forEach((section) => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top <= window.innerHeight / 3) {
+            currentSection = section.id;
         }
-    })
-});  
+    });
+    let position_active = arrPositionModel.findIndex(
+        (val) => val.id == currentSection
+    );
+    if (position_active >= 0) {
+        let new_coordinates = arrPositionModel[position_active];
+        gsap.to(bee.position, {
+            x: new_coordinates.position.x,
+            y: new_coordinates.position.y,
+            z: new_coordinates.position.z,
+            duration: 3,
+            ease: "power1.out"
+        });
+        gsap.to(bee.rotation, {
+            x: new_coordinates.rotation.x,
+            y: new_coordinates.rotation.y,
+            z: new_coordinates.rotation.z,
+            duration: 3,
+            ease: "power1.out"
+        })
+    }
+}
+window.addEventListener('scroll', () => {
+    if (bee) {
+        modelMove();
+    }
+})
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+})
